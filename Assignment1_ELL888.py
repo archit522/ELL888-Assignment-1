30becb990c99d3460f8aca8a751a3a4aa91dd24a
Akash Pardasani <me1150629@ccl3pc08.cc.iitd.ernet.in>
2018-01-24 16:30:43 +0530

    Adding cost functions and batch division part

--- Assignment1_ELL888.py
+++ Assignment1_ELL888.py
@@ -19,10 +19,18 @@ class NeuralNet:
 
 
 	def sigmoid(self, x):         # sigmoid activation function
-		return (1.0/(1.0 + math.exp(-x)))
+		return (1.0/(1.0 + np.exp(-x)))
 
 	def sigmoid_derivative(self,x):  #sigmoid derivative function (for backpropagation)
-		return (math.exp(-x)/((1 + math.exp(-x)) ** 2))
+		return (np.exp(-x)/((1 + math.exp(-x)) ** 2))
+
+	def divide_and_learn(self, training_set, num_batches, batch_size):
+		batches = []
+		for i in np.arange(0, len(training_set), num_batches):
+			batches.append(training_set[i:i+num_batches])
+		
+
+
 
 	def backprop(self, labels):
 		delta_L = del_cost(self.activations[-1], labels) * sigmoid_derivative(self.z[-1])
@@ -32,8 +40,7 @@ class NeuralNet:
 		for i in range(len(self.weights)):
 			delta.insert(0,(np.dot(self.weights[-i-1].T, delta[-1-i])*sigmoid_derivative(self.z[-i-2])))
 		for j in range(len(self.weights) - 1):		
-			del_weights.insert(0, (np.dot(delta[-i-2], self.activations[-i-3].T))) 
-		
+			del_weights.insert(0, (np.dot(delta[-i-2], self.activations[-i-3].T))) 		
 		return del_weights
 
 	def learn_NN(self, X, labels, eta, batch_size):
@@ -43,6 +50,29 @@ class NeuralNet:
 			del_weight = self.backprop(_label)
 			add_weight = add_weight + eta*del_weight
 		self.weights = self.weights - (add_weight/batch_size)
+
+	def cost_function_MSE_noReg(self, label):
+		cost = np.zeros(np.shape(self.activations[-1]))
+		cost = (self.activations[-1] - label)**2
+		return np.sum(cost)
+
+	def cost_derivative_MSE_noReg(self, label):
+		cost_der = np.zeros(np.shape(self.activations[-1]))
+		cost_der = self.activations[-1] - label
+		return cost_der
+
+	def cost_function_CEntropy_noReg(self, label):
+		cost = np.zeros(np.shape(self.activations[-1]))
+		inv_activations = np.ones(np.shape(self.activations[-1])) - self.activations[-1]
+		inv_label = np.ones(np.shape(y)) - y
+		cost = label*np.log(self.activations[-1]) + inv_label*np.log(inv_activations)
+		return -1*np.sum(cost)
+
+	def cost_derivative_CEntropy_noReg(self, label):
+		cost_der = np.zeros(np.shape(self.activations[-1]))
+		
+
+	
 
 	
 			
